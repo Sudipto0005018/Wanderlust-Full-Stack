b33a9d26555bf4946ad62747b000e3e41836dda4
Sudipto Dutta <duttasudipto11@gmail.com>
2025-02-16 12:22:07 +0530

    new changes

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -1,15 +1,30 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// const MONGODB_URL = process.env.MONGODB_CONNECT_URL;
+
+// const dbconnect = mongoose.connect( MONGODB_URL, (
+//     console.log('Database successfully connected')
+// ));
+
+// module.exports = dbconnect;
+
+
+
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const MONGODB_URL = process.env.MONGODB_CONNECT_URL;
 
-const dbconnect = mongoose.connect( MONGODB_URL, (
-    console.log('Database successfully connected')
-));
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database successfully connected');
+    })
+    .catch((err) => {
+        console.error('Database connection error:', err);
+    });
 
-module.exports = dbconnect;
-
-
-
+module.exports = mongoose;
